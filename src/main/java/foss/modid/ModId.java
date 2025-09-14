2ca14891b096a3a7f3baddebda99c7dc4e5e22f8
CaliforniaDemise <vwwwwv@proton.me>
2025-09-14 21:58:19 +0300

    Implement a basic template.

---
+++ src/main/java/foss/modid/ModId.java
@@ -0,0 +1,15 @@
+package foss.modid;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
+public class ModId {
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        Logger logger = event.getModLog();
+        logger.info("Hello from PreInit!");
+    }
+}
